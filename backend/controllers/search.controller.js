fab288a8e9e5317187a31295bb6dab27a2b2910f
Mr-robot <hackingtest017@gmail.com>
2024-10-06 11:35:46 +0300

    auth phase

--- backend/controllers/search.controller.js
+++ backend/controllers/search.controller.js
@@ -1,7 +1,14 @@
-
+import { StatusCodes } from 'http-status-codes';
+import CustomError from '../errors/index.js';
+import Keyword from '../models/Keyword.js';
 
 const SearchController = {
-
+    async suggestion(req, res) {
+        const { query = null } = req.query;
+        if (!query) {
+            throw new CustomError.BadRequest('query must be specified')
+        }
+    }
 }
 
 export default SearchController;
