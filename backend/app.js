8192b9b11952d627d5c1eb084e22669db8dd61db
Mr-robot <hackingtest017@gmail.com>
2024-10-09 21:20:42 +0300

    add  dep log

--- backend/app.js
+++ backend/app.js
@@ -62,6 +62,7 @@ const main = async () => {
 
     app.listen(config.PORT, () => {
         console.log(`Server is running on port 5000`);
+        console.log(`Worker must run to handle thumbnail generation`);
     });
 }
 
